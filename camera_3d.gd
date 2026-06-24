f2251aecc34b386806bd49baf05841e6e48e086c
Tal1762 <113717843+Tal1762@users.noreply.github.com>
2026-06-24 17:49:47 -0500

    Lighting physics fixes

---
+++ camera_3d.gd
@@ -0,0 +1,17 @@
+extends Camera3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+@export var look_target: Node3D
+@export var follow_speed: float = 5.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# Smoothly move towards target position
+	global_position = global_position.lerp(Vector3(look_target.global_position.x, position.y, position.z), follow_speed * delta)
+
+	# Smoothly rotate to look at target
+	#look_at(look_target.global_position)
--- top.gd
+++ top.gd
@@ -1,11 +1,13 @@
 extends RigidBody3D
 
 @export var body:RigidBody3D;
-@export var stability_factor: float = .3
+@export var stability_factor: float = .7
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	body.add_constant_force(Vector3(10, 0, 0))
 	body.apply_torque(Vector3(0, 10000, 0))
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
