74c56bc1b33ac74660d5f9ed1ac0471e1968ba03
Tal1762 <113717843+Tal1762@users.noreply.github.com>
2026-06-24 16:34:08 -0500

    Top+cam fix

--- top.gd
+++ top.gd
@@ -5,6 +5,7 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	body.add_constant_torque(Vector3(0, 200, 0))
+	body.add_constant_force(Vector3(10, 0 ,0))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
