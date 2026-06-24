433dc4121adb9a8a137efffea8d7c49281294a20
Jacob <jacob612r@gmail.com>
2026-06-24 18:11:13 -0500

    Force adjustments

--- top.gd
+++ top.gd
@@ -1,16 +1,11 @@
 extends RigidBody3D
 
-@export var body:RigidBody3D;
 @export var stability_factor: float = .7
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-<<<<<<< HEAD
-	body.apply_force(Vector3(1000, 0, 0))
-=======
 	add_constant_force(Vector3(10, 0 ,0))
->>>>>>> 78b5a96bc6da8b56ede78287254607c3857d253f
-	body.apply_torque(Vector3(0, 10000, 0))
+	#body.apply_torque(Vector3(0, 10000, 0))
 	
 
 
@@ -20,13 +15,15 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	#pass
-	body.apply_torque(Vector3(0, 100, 0))
+	apply_torque(Vector3(0, 100, 0))
 	gyroscopic_forces()
+	apply_central_force(Vector3(10, 0 ,0))
+	
 
 func gyroscopic_forces() -> void:
-	var spin_speed:float = abs(body.angular_velocity.y)
-	var rel_up = body.transform.basis.y
+	var spin_speed:float = abs(angular_velocity.y)
+	var rel_up = transform.basis.y
 	var up_torque = rel_up.cross(Vector3.UP)
 	var gyro_torque = up_torque * spin_speed * stability_factor
-	body.apply_torque(gyro_torque)
+	apply_torque(gyro_torque)
 	
