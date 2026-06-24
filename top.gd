3dce04991c1d436bcd1a3d7221aee2bbe5599b0d
Jacob <jacob612r@gmail.com>
2026-06-24 18:52:42 -0500

    Movement and friction

--- top.gd
+++ top.gd
@@ -1,16 +1,12 @@
 extends RigidBody3D
 
-@export var stability_factor: float = .7
+@export var stability_factor:float = .7
+@export var terminal_velocity:float = 15.0
+@export var velocity:float = 15.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	add_constant_force(Vector3(10, 0 ,0))
-<<<<<<< HEAD
-	body.apply_torque(Vector3(0, 10000, 0))
-=======
-	#body.apply_torque(Vector3(0, 10000, 0))
->>>>>>> 16064e77baeb5e5feb4bcc3adcf226b7421b0e86
-	
+	apply_torque(Vector3(0, 10000, 0))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,9 +15,13 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	#pass
-	apply_torque(Vector3(0, 100, 0))
+	apply_torque(Vector3(0, 10, 0))
+	friction()
+	movement()
 	gyroscopic_forces()
-	apply_central_force(Vector3(10, 0 ,0))
+	
+	if (abs(linear_velocity.x) > terminal_velocity):
+		linear_velocity.x = sign(linear_velocity.x) * 10
 	
 
 func gyroscopic_forces() -> void:
@@ -31,3 +31,15 @@ func gyroscopic_forces() -> void:
 	var gyro_torque = up_torque * spin_speed * stability_factor
 	apply_torque(gyro_torque)
 	
+func movement() -> void:
+	if Input.is_action_pressed("left"):
+		apply_central_force(Vector3(-velocity, 0 ,0))
+	if Input.is_action_pressed("right"):
+		apply_central_force(Vector3(velocity, 0 ,0))
+	
+func friction() -> void:
+	if linear_velocity.x == 0:
+		return
+	if abs(linear_velocity.x) < 0.1:
+		linear_velocity.x = 0
+	apply_central_force(Vector3(-10 * linear_velocity.x / 7, 0 ,0))
