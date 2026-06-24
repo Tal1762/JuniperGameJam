1246b41c65d4f35b2072d9a538aab4790cc02c26
Jacob <jacob612r@gmail.com>
2026-06-24 16:23:42 -0500

    Added spin to script

--- top.gd
+++ top.gd
@@ -1,11 +1,15 @@
 extends Node3D
 
+@export var body:RigidBody3D;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	body.add_constant_torque(Vector3(0, 200, 0))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
 	pass
