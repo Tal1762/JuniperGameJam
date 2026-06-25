4a75f8dbc43d069b5c5f91f338bc7c48ae49c778
Tal1762 <113717843+Tal1762@users.noreply.github.com>
2026-06-24 20:23:11 -0500

    tornado fixes

--- layer.gd
+++ layer.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	rotation.y += 1
+	rotation.y += 0.05
--- tornado.gd
+++ tornado.gd
@@ -13,3 +13,4 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(top: Top) -> void:
 	top.inc_pseudo_speed(speed_inc)
+	queue_free()
